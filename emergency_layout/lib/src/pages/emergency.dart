dfcf6479d4219c9fe9cf67cd4efbdab55a62c9ad
wallas <whuanca06@gmail.com>
2020-08-08 00:32:15 -0400

    adding emergency_layout app

---
+++ emergency_layout/lib/src/pages/emergency.dart
@@ -0,0 +1,107 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:emergency_layout/src/widgets/boton_gordo.dart';
+import 'package:emergency_layout/src/widgets/headers.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ItemBoton {
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton(this.icon, this.texto, this.color1, this.color2);
+}
+
+class EmergencyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final items = <ItemBoton>[
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+      new ItemBoton(FontAwesomeIcons.carCrash, 'Motor Accident',
+          Color(0xff6989F5), Color(0xff906EF5)),
+      new ItemBoton(FontAwesomeIcons.plus, 'Medical Emergency',
+          Color(0xff66A9F2), Color(0xff536CF6)),
+      new ItemBoton(FontAwesomeIcons.theaterMasks, 'Theft / Harrasement',
+          Color(0xffF2D572), Color(0xffE06AA3)),
+      new ItemBoton(FontAwesomeIcons.biking, 'Awards', Color(0xff317183),
+          Color(0xff46997D)),
+    ];
+
+    List<Widget> itemMap = items
+        .map((item) => FadeInLeft(
+              child: BotonGordo(
+                texto: item.texto,
+                icon: item.icon,
+                onPress: () {
+                  print('pichi');
+                },
+                color1: item.color1,
+                color2: item.color2,
+              ),
+            ))
+        .toList();
+    return Scaffold(
+        body: Stack(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 200),
+          child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: <Widget>[
+              SizedBox(
+                height: 80,
+              ),
+              ...itemMap
+            ],
+          ),
+        ),
+        _Encabezado()
+      ],
+    ));
+  }
+}
+
+class _Encabezado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        IconHeader(
+          icon: FontAwesomeIcons.plus,
+          titulo: 'Asistencia Medica',
+          subtitulo: 'Haz Solicitado',
+          color1: Color(0xff536CF6),
+          color2: Color(0xff66A9F2),
+        ),
+        Positioned(
+            right: 0,
+            top: 45,
+            child: RawMaterialButton(
+              onPressed: () {},
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(15),
+              child: FaIcon(
+                FontAwesomeIcons.ellipsisV,
+                color: Colors.white,
+              ),
+            ))
+      ],
+    );
+  }
+}
